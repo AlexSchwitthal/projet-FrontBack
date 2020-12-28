a8f320adea6b8c64015e2ef52521b496fbe70326
liamEponym <kormannliam@gmail.com>
2020-12-28 22:15:59 +0100

    Front follow

--- front/src/app/profile/profile.component.ts
+++ front/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../models/user';
+import { AuthService } from '../services/auth.service';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -11,13 +12,16 @@ import { UsersService } from '../services/users.service';
 export class ProfileComponent implements OnInit {
 
   login: any;
-  userPage: User;
+  userPage: User = null;
   isValidUser : boolean;
+  isOwnProfile : boolean;
+  isFollowing: boolean;
 
-  constructor(public usersService : UsersService, private route: ActivatedRoute, private router: Router) { 
+  constructor(public usersService : UsersService, private route: ActivatedRoute, private router: Router, public authService: AuthService) { 
     route.params.subscribe(val => {
       this.login = val.username;
       this.getUser(this.login);
+      this.checkOwnProfile(this.login);       
     });
   }
 
@@ -30,6 +34,7 @@ export class ProfileComponent implements OnInit {
       (user:any) => {
         this.userPage = new User(user._id, user.login, user.password, user.nickname, user.following);
         this.isValidUser = true;
+        this.checkFollowing();    
       },
       (error) => {
         this.userPage = null;
@@ -37,5 +42,21 @@ export class ProfileComponent implements OnInit {
         console.log(error);
       }
     )
-	}
+  }
+  
+  checkOwnProfile(login:any):any{
+    if (this.login === this.authService.connectedUser.login){
+      this.isOwnProfile = true;
+    }
+    else this.isOwnProfile = false;
+  }
+
+  checkFollowing():any{
+    if (this.authService.connectedUser.following.includes(this.userPage._id)){
+      this.isFollowing=true;
+    }
+    else{
+      this.isFollowing=false;
+    }
+  }
 }
