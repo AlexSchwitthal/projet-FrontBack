01f1057d8919ad546538026c6e927300354a12d3
Calliane <slene.tan@gmail.com>
2020-12-30 16:31:32 +0100

    new design

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,23 @@ import { AuthService } from '../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private router: Router) { }
 
 
   ngOnInit(): void {
+  }
+
+  logout() : void {
+    this.authService.logout().subscribe(
+      () => {
+      },
+      (error) => {
+      }
+    )
+  }
+
+  profile(): void {
+    this.router.navigate(['/profile', this.authService.connectedUser.login]);
   }
 
 }
